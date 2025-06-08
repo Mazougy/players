1f8d7070df9d72c62d003cbdeaafc567e139fe62
Abas-Almas <marzouguiadam@fsm.u-monastir.tn>
2025-06-08 22:24:57 +0100

    main

--- client.lua
+++ client.lua
@@ -1,11 +1,11 @@
 addCommandHandler("respawn", function()
     local Player = getPlayerName(localPlayer)
 
-    setElementPosition(localPlayer, 1, 5, 0)
+    setElementPosition(localPlayer, 0, 0, 5)
 
     local money = getPlayerMoney(localPlayer)
 
-    setPlayerMoney(money + 1500, true)
+    setPlayerMoney(money + 1500)
 
     outputChatBox(Player .. " successfully respawned")
 end)
